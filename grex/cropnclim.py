4ac92674f223881ea95b36a958adc40c66244b7d
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-24 14:48:46 +0330

    separate california from all us shape file

--- grex/cropnclim.py
+++ grex/cropnclim.py
@@ -5,3 +5,5 @@ import geopandas as gpd
 # Source: https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
 
 us = gpd.read_file("../dataset/censusshape/tl_2025_us_state.shp")
+ca = us[us['NAME'] == 'California']
+ca = ca.to_crs("EPSG:4326")
