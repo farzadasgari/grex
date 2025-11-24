895764f2717ceb594aa42ea8964d1ccde41ebbbf
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-24 14:52:02 +0330

    test of cropping to california for one grid data

--- grex/cropnclim.py
+++ grex/cropnclim.py
@@ -7,3 +7,8 @@ import geopandas as gpd
 us = gpd.read_file("../dataset/censusshape/tl_2025_us_state.shp")
 ca = us[us['NAME'] == 'California']
 ca = ca.to_crs("EPSG:4326")
+
+ds = xr.open_dataset("../dataset/nclimgrid/ncdd-195101-grd-scaled.nc")
+ds = ds.rio.write_crs("EPSG:4326")
+ds_ca = ds.rio.clip(ca.geometry, ca.crs)
+ds_ca.to_netcdf("tmax_CA.nc")
