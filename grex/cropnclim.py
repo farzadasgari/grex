ce5a790ed2be438c575157a051e0f917f1afe5e2
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-24 15:15:28 +0330

    automating cropping processing

--- grex/cropnclim.py
+++ grex/cropnclim.py
@@ -1,14 +1,23 @@
 import xarray as xr
 import rioxarray
 import geopandas as gpd
+from pathlib import Path
 
 # Source: https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
 
-us = gpd.read_file("../dataset/censusshape/tl_2025_us_state.shp")
-ca = us[us['NAME'] == 'California']
-ca = ca.to_crs("EPSG:4326")
+src_dir = Path("../dataset/nclimgrid/")
+dst_dir = Path("../dataset/croppednclimgrid/")
+dst_dir.mkdir(exist_ok=True, parents=True)
 
-ds = xr.open_dataset("../dataset/nclimgrid/ncdd-195101-grd-scaled.nc")
-ds = ds.rio.write_crs("EPSG:4326")
-ds_ca = ds.rio.clip(ca.geometry, ca.crs)
-ds_ca.to_netcdf("tmax_CA.nc")
+us = gpd.read_file("../dataset/censusshape/tl_2025_us_state.shp")
+ca = us[us['NAME'] == 'California'].to_crs("EPSG:4326")
+
+for nc_file in src_dir.glob("*.nc"):
+    print(f"Cropping → {nc_file.name}")
+    ds = xr.open_dataset(nc_file, chunks={"time": 365})
+    ds = ds.rio.write_crs("EPSG:4326", inplace=True)
+    ds_ca = ds.rio.clip(ca.geometry, ca.crs)
+    out_name = f"ca-{nc_file.name}"
+    out_path = dst_dir / out_name
+    ds_ca.to_netcdf(out_path)
+
