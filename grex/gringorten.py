2d336d2cda33c988933421ce8645fc85dd51ced0
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-29 21:24:50 +0330

    choose a random cell for sdhi

--- grex/gringorten.py
+++ grex/gringorten.py
@@ -6,3 +6,10 @@ warm_da = xr.open_dataarray("../dataset/warm_seasons_ca.nc")
 
 mask = ~np.isnan(warm_da).any(dim='window')
 lat_idxs, lon_idxs = np.where(mask)
+
+if len(lat_idxs) == 0:
+    raise "No valid cells found — check file."
+
+rnd_idx = np.random.choice(len(lat_idxs))
+selected_lat = warm_da.lat[lat_idxs[rnd_idx]].values
+selected_lon = warm_da.lon[lon_idxs[rnd_idx]].values
