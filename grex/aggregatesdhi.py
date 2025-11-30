c6b75ef557d4d51a44bc860912957c2984084554
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-30 14:01:53 +0330

    calculate metrics of sdhi through years

--- grex/aggregatesdhi.py
+++ grex/aggregatesdhi.py
@@ -13,3 +13,15 @@ thresholds = {
 
 land_mask = ~np.isnan(sdhi_da.mean(dim='year'))
 total_land_cells = land_mask.sum().item()
+
+metrics = []
+for year in sdhi_da.year.values:
+    year_sdhi = sdhi_da.sel(year=year)
+    valid_year_mask = ~np.isnan(year_sdhi)
+    year_land_cells = valid_year_mask.sum().item()
+    row = {'year': year, 'mean_sdhi': year_sdhi.mean().values}
+    for level, thresh in thresholds.items():
+        below = (year_sdhi <= thresh) & valid_year_mask
+        pct = (below.sum().item() / year_land_cells) * 100 if year_land_cells > 0 else np.nan
+        row[f'pct_{level}'] = pct
+    metrics.append(row)
