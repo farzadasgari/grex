1138e44fa377b7a22955c397271cf61d6c90f8e3
Farzad Asgari <khufarzadasgari@gmail.com>
2025-12-01 00:21:30 +0330

    calculate scei for all cells through years

--- grex/sceidataset.py
+++ grex/sceidataset.py
@@ -83,3 +83,55 @@ data_dict = {
     v: np.full((n_years, len(lats), len(lons)), np.nan, dtype=float)
     for v in vars_to_save
 }
+
+for c_idx in tqdm(range(len(lat_idxs)), desc="Cells"):
+    i_lat = lat_idxs[c_idx]
+    i_lon = lon_idxs[c_idx]
+
+    lat_val = lats[i_lat]
+    lon_val = lons[i_lon]
+
+    warm_months = warm_da.isel(lat=i_lat, lon=i_lon).values.astype(int)
+    cell = monthly_ds[["prcp", "tmean"]].sel(lat=lat_val, lon=lon_val, method="nearest")
+
+    P_warm = np.full(n_years, np.nan)
+    T_warm = np.full(n_years, np.nan)
+
+    for y_idx, year in enumerate(years):
+        ydata = cell.sel(time=cell["time.year"] == year)
+        if ydata.time.size == 0:
+            continue
+
+        months = ydata["time.month"].values
+        m_mask = np.isin(months, warm_months)
+        if m_mask.sum() != 3: continue
+        P_vals = ydata["prcp"].values[m_mask]
+        T_vals = ydata["tmean"].values[m_mask]
+
+        P_warm[y_idx] = P_vals.sum()
+        T_warm[y_idx] = T_vals.mean()
+
+    valid_warm = ~(np.isnan(P_warm) | np.isnan(T_warm))
+    if valid_warm.sum() < 10: continue
+    P_valid = P_warm[valid_warm]
+    T_valid = T_warm[valid_warm]
+
+    SPI_valid = standardized_index_from_series(P_valid)
+    STI_valid = standardized_index_from_series(T_valid)
+
+    SPI = np.full_like(P_warm, np.nan)
+    STI = np.full_like(T_warm, np.nan)
+
+    full_valid_idxs = np.where(valid_warm)[0]
+    SPI[full_valid_idxs] = SPI_valid
+    STI[full_valid_idxs] = STI_valid
+    SCEI = scei_from_spi_sti(SPI, STI)
+
+    for y_idx in range(n_years):
+        if np.isnan(P_warm[y_idx]) or np.isnan(T_warm[y_idx]):
+            continue
+        data_dict["P_warm"][y_idx, i_lat, i_lon] = P_warm[y_idx]
+        data_dict["Tmean_warm"][y_idx, i_lat, i_lon] = T_warm[y_idx]
+        data_dict["SPI"][y_idx, i_lat, i_lon] = SPI[y_idx]
+        data_dict["STI"][y_idx, i_lat, i_lon] = STI[y_idx]
+        data_dict["SCEI"][y_idx, i_lat, i_lon] = SCEI[y_idx]
