f0f706d983ea67030a9820171001fe23a7809c05
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-30 13:51:07 +0330

    compute sdhi based on x gringorten values for all cells

--- grex/sdhi.py
+++ grex/sdhi.py
@@ -1,5 +1,7 @@
 import xarray as xr
 import numpy as np
+from scipy.stats import rankdata, norm
+from tqdm import tqdm
 
 x_da = xr.open_dataarray("../dataset/x_ca_1951_2025.nc")
 
@@ -12,3 +14,17 @@ years = x_da.year.values
 n_years = len(years)
 
 sdhi_arr = np.full((n_years, len(x_da.lat), len(x_da.lon)), np.nan)
+
+for c_idx in tqdm(range(n_cells), desc="Processing cells"):
+    sel_lat_idx = lat_idxs[c_idx]
+    sel_lon_idx = lon_idxs[c_idx]
+    cell_x = x_da.isel(lat=sel_lat_idx, lon=sel_lon_idx).values
+    valid_mask = ~np.isnan(cell_x)
+    if np.sum(valid_mask) < 10: continue
+    x_valid = cell_x[valid_mask]
+    n_valid = len(x_valid)
+    rank_x = rankdata(x_valid, method='average')
+    f_x = (rank_x - 0.44) / (n_valid + 0.12)
+    f_x = np.clip(f_x, 1e-6, 1 - 1e-6)
+    sdhi_valid = norm.ppf(f_x)
+    sdhi_arr[valid_mask, sel_lat_idx, sel_lon_idx] = sdhi_valid
