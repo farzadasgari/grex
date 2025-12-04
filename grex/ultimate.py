63905e50a4b9cd9974a7d706d6fd72732df4ec24
Farzad Asgari <khufarzadasgari@gmail.com>
2025-12-04 23:08:57 +0330

    update ultimate dataset exporter for monthly scale

--- grex/ultimate.py
+++ grex/ultimate.py
@@ -1,7 +1,7 @@
 import xarray as xr
 
-ds_sdhi = xr.open_dataset("../dataset/ca_sdhi_1951_2025.nc")
-ds_scei = xr.open_dataset("../dataset/ca_scei_1951_2025.nc")
+ds_sdhi = xr.open_dataset("../dataset/ca_sdhi_warmest_1951_2025.nc")
+ds_scei = xr.open_dataset("../dataset/ca_scei_warmest_1951_2025.nc")
 
 ds_sdhi_renamed = ds_sdhi.rename({
     "prcp": "prcp_sdhi",
@@ -30,5 +30,41 @@ ds_merged.attrs["title"] = "California Compound Climate Extremes — SDHI + SCEI
 ds_merged.attrs["author"] = "Farzad Asgari"
 ds_merged.attrs["source"] = "NOAA nClimGrid Gridded Dataset"
 
-out = "../dataset/california.nc"
+out = "../dataset/california_warmest.nc"
 ds_merged.to_netcdf(out)
+
+ds_sdhi = xr.open_dataset("../dataset/ca_sdhi_monthly_1951_2025.nc")
+ds_scei = xr.open_dataset("../dataset/ca_scei_monthly_1951_2025.nc")
+
+ds_sdhi_renamed = ds_sdhi.rename({
+    "pet": "pet_sdhi",
+    "spi": "spi_sdhi",
+    "sti": "sti_sdhi",
+    "spei": "spei_sdhi",
+    "x1": "x1_sdhi",
+    "x2": "x2_sdhi",
+})
+
+ds_scei_renamed = ds_scei.rename({
+    "spi": "spi_scei",
+    "sti": "sti_scei",
+    "scei": "scei"
+})
+
+ds_sdhi_selected = ds_sdhi_renamed[[
+    "pet_sdhi", "prcp", "sdhi1", "sdhi2", "spei_sdhi", "spi_sdhi", "sti_sdhi", "tmax", "tmean", "tmin", "x1_sdhi", "x2_sdhi", "year"
+]]
+
+ds_scei_selected = ds_scei_renamed[[
+    "spi_scei", "sti_scei", "scei"
+]]
+
+ds_merged = xr.merge([ds_sdhi_selected, ds_scei_selected])
+
+ds_merged.attrs["title"] = "California Compound Climate Extremes — SDHI + SCEI (1951–2025)"
+ds_merged.attrs["author"] = "Farzad Asgari"
+ds_merged.attrs["source"] = "NOAA nClimGrid Gridded Dataset"
+
+out = "../dataset/california_monthly.nc"
+ds_merged.to_netcdf(out)
+
